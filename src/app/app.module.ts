3449728f499ac1534c2cefa0028bd8f5beea03e0
Mika Rafaralahy <mrafa099@uottawa.ca>
2022-06-04 17:17:39 -0500

    HTML + squeletal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { LocationComponent } from './location/location.component';
+import { ExpertsComponent } from './experts/experts.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    AboutComponent,
+    LocationComponent,
+    ExpertsComponent,
+    BookingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
